7040fe359f129253d38c53d016336a72e2d2badd
Jesús Gómez <jgomo3@gmail.com>
2016-12-19 16:20:01 -0400

    Implements the is_prime method

--- primes.py
+++ primes.py
@@ -1,16 +1,34 @@
 """
-Your module documentation here
+Collection of definitions useful for prime number manipulation.
+
+classes:
+- PrimeClass
 """
+
+import math
+
+from itertools import chain
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Definition of a Utility useful for manipulating prime numbers, like
+    checking if one number is prime.
+
+    methods:
+    - is_prime
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Determine if the given number is prime.
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        return num_int > 1 and (
+            num_int == 2 or not any(
+                num_int % divisor == 0
+                for divisor in chain(
+                    (2,),
+                    xrange(3, int(math.sqrt(num_int)) + 1, 2))
+            )
+        )
